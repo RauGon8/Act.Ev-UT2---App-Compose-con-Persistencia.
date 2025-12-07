294e68335b387afe9afb871a3c279c9bcf21e1c8
colwhinamp <penaherediaandresmauricio@gmail.com>
2025-12-07 07:53:15 +0000

    initial commmit andres
    estructura ui

---
+++ app/src/main/java/com/example/act2ens_aprprimercontactoconjetpackcompose/ui/theme/ui/peliculas/PeliculasViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.act2ens_aprprimercontactoconjetpackcompose.ui.theme.ui.peliculas
+
+class PeliculasViewModel {
+}
---
+++ app/src/main/java/com/example/act2ens_aprprimercontactoconjetpackcompose/ui/theme/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.act2ens_aprprimercontactoconjetpackcompose.ui.theme.ui.settings
+
+class SettingsViewModel {
+}
