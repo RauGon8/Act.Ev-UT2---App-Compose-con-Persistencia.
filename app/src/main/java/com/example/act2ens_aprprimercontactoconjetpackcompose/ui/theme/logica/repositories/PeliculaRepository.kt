e7352f52b2c2fa85de9d3ee51d067fde3c2956a9
RauGon8 <raulgonzalezherrera8@gmail.com>
2025-12-08 21:25:26 +0000

    Logica del proyecto
    (faltaria todo el tema de la UI)

--- app/src/main/java/com/example/act2ens_aprprimercontactoconjetpackcompose/ui/theme/logica/repositories/PeliculaRepository.kt
+++ app/src/main/java/com/example/act2ens_aprprimercontactoconjetpackcompose/ui/theme/logica/repositories/PeliculaRepository.kt
@@ -6,10 +6,10 @@ import kotlinx.coroutines.flow.Flow
 
 class PeliculaRepository(private val peliculaDao: PeliculaDao) {
 
-    // Exponemos el flujo de datos de la base de datos
+
     val peliculas: Flow<List<PeliculaEntity>> = peliculaDao.obtenerTodas()
 
-    // Función para AGREGAR (Create)
+
     suspend fun agregarPelicula(titulo: String, genero: String) {
         val nuevaPelicula = PeliculaEntity(
             titulo = titulo,
@@ -24,12 +24,11 @@ class PeliculaRepository(private val peliculaDao: PeliculaDao) {
         peliculaDao.actualizar(pelicula)
     }
 
-    // Función para BORRAR (Delete)
+
     suspend fun borrarPelicula(pelicula: PeliculaEntity) {
         peliculaDao.borrar(pelicula)
     }
 
-    // Función para cambiar FAVORITO
     suspend fun toggleFavorito(pelicula: PeliculaEntity) {
         // Copiamos el objeto invirtiendo el valor de isFavorite
         val peliculaActualizada = pelicula.copy(isFavorite = !pelicula.isFavorite)
